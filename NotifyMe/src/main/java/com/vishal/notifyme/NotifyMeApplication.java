bdc820cebd8a03e3085d983706cff00a48457671
vishalsharma2502 <contact2vishalsharma@gmail.com>
2023-04-05 21:56:44 +0530

    adding strategy to splitwise

---
+++ NotifyMe/src/main/java/com/vishal/notifyme/NotifyMeApplication.java
@@ -0,0 +1,13 @@
+package com.vishal.notifyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotifyMeApplication.class, args);
+    }
+
+}
---
+++ NotifyMe/src/test/java/com/vishal/notifyme/NotifyMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishal.notifyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
